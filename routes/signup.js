3ff9226a4384a50e081dddf20b67d092679ba912
anthonyjanocko <anthonyjjanocko@gmail.com>
2015-09-05 13:37:25 -0400

    Created separate files for college/student sign up

--- routes/signup.js
+++ routes/signup.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('signup');
+router.get('/college', function(req, res, next) {
+  res.render('signup-college');
+});
+
+router.get('/student', function(req, res, next) {
+  res.render('signup-student');
 });
 
 module.exports = router;
