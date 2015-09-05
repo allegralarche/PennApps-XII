2f36ac1ce928da56bb323f5ad60751fb7a958110
Allegra Larche <allegralarche@gmail.com>
2015-09-05 12:13:45 -0400

    styling for login and home page

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Squads' });
+  res.render('pages/index', { title: 'School Squad' });
 });
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('signup');
+  res.render('pages/signup');
 });
 
 module.exports = router;
