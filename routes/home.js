b1222549effe6525ef65f7a26119956e47757716
Sarah Krueger <skrueger@seas.upenn.edu>
2015-09-06 07:22:24 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	views/pages/login.ejs

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'School Squad' });
+  res.render('pages/index', { title: 'schoolsquad' });
 });
 
 module.exports = router;
